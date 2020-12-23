6862357c9c37ea3fbd04f1592a7e561da5f293b9
SinGlEBW <bw.klan@mail.ru>
2020-12-23 17:58:21 +0300

    l2

--- src/js/dev/axios.js
+++ src/js/dev/axios.js
@@ -1,1 +1,15 @@
 import axios from 'axios';
+
+//graph.instagram.com  //запрос на медиа файлы
+
+/*
+
+12503711546  - id шурика
+
+  В get запросе передать список полей можно
+  ?fields={fields}
+  &access_token={access-token}
+
+timestamp - дата публикации
+
+*/
